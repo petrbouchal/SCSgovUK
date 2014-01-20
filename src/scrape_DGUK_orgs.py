f696c67f5b5b85d39df93ca1709aa5458129bb53
Petr Bouchal <pbouchal@gmail.com>
2014-01-20 11:50:50 +0000

    Initial code for downloading list of organisations

---
+++ src/scrape_DGUK_orgs.py
@@ -0,0 +1,28 @@
+__author__ = 'bouchalp'
+
+import urllib2
+import json
+from pprint import pprint
+
+apibase = 'http://data.gov.uk/api/3'
+limit=1000
+
+searchterm = ''
+
+action = '/action/organization_list?all_fields=true'
+query = apibase + action + '&rows=' + str(limit)
+print(query)
+
+data = urllib2.urlopen(query).read()
+jdata=json.loads(data)
+pprint(jdata['result'])
+
+orgaction = '/action/organization_show'
+
+for org in jdata['result']:
+    orgquery = apibase + orgaction + '?id=' + org['id']
+    print(orgquery)
+    orgdata = urllib2.urlopen(orgquery).read()
+    jorgdata = json.loads(orgdata)
+    print(jorgdata['result']['title'])
+
